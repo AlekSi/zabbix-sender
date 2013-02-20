80ad6f54ea502756fe07173859d2d49628cf65ae
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2013-02-20 23:23:31 +0400

    Initial commit.

---
+++ sender.go
@@ -0,0 +1,55 @@
+package zabbix_sender
+
+import (
+	"bytes"
+	"encoding/binary"
+	"encoding/json"
+	"fmt"
+)
+
+var (
+	header = []byte("ZBXD\x01")
+)
+
+type dataItem struct {
+	Host  string `json:"host"`
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
+
+type Sender struct {
+	Host string
+}
+
+func convertValue(i interface{}) string {
+	switch v := i.(type) {
+	case float32, float64:
+		return fmt.Sprintf("%f", v)
+	default:
+		return fmt.Sprint(v)
+	}
+	panic("not reached")
+}
+
+func (s *Sender) Convert(kv map[string]interface{}) (b []byte, err error) {
+	data := make([]dataItem, len(kv))
+	i := 0
+	for k, v := range kv {
+		data[i] = dataItem{s.Host, k, convertValue(v)}
+		i++
+	}
+
+	d, err := json.Marshal(data)
+	if err == nil {
+		l := uint64(len(d))
+		b = make([]byte, 0, 45+l) // 5 + 8 + 31 + l + 1
+		buf := bytes.NewBuffer(b)
+		buf.Write(header)                                  // 5
+		err = binary.Write(buf, binary.LittleEndian, l)    // 8
+		buf.WriteString(`{"request":"agent data","data":`) // 31
+		buf.Write(d)                                       // l
+		buf.WriteByte('}')                                 // 1
+		b = buf.Bytes()
+	}
+	return
+}
---
+++ sender_test.go
@@ -0,0 +1,89 @@
+package zabbix_sender_test
+
+import (
+	. "."
+	"encoding/json"
+	"fmt"
+	"strings"
+	"testing"
+)
+
+func TestConvert(t *testing.T) {
+	var (
+		sender         = &Sender{"localhost"}
+		i      int     = 42
+		f0     float32 = 0
+		f1     float32 = 1
+		f32    float32 = 3.141592653
+		f64    float64 = 3.141592653
+		s              = "string"
+		e      error   = fmt.Errorf("error")
+	)
+	b, err := sender.Convert(map[string]interface{}{"int": i, "f0": f0, "f1": f1, "float32": f32, "float64": f64, "s": s, "e": e})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(b) != cap(b) {
+		t.Errorf("Optimize: len(%d) != cap(%d)", len(b), cap(b))
+	}
+
+	if string(b[0:5]) != "ZBXD\x01" {
+		t.Error("Wrong header")
+	}
+	if string(b[5:13]) != "\x64\x01\x00\x00\x00\x00\x00\x00" {
+		t.Errorf("Wrong size: %x", b[5:13])
+	}
+
+	b = b[13:]
+	str := string(b)
+	if !strings.HasPrefix(str, `{"request":"agent data","data":[{`) ||
+		strings.Contains(str, `, `) ||
+		strings.Contains(str, `: `) {
+		t.Error("Zabbix's JSON parser will not parse it:", str)
+	}
+
+	var m map[string]interface{}
+	err = json.Unmarshal(b, &m)
+	if err != nil {
+		t.Fatal(err)
+	}
+	data := m["data"].([]interface{})
+	for i := range data {
+		d := data[i].(map[string]interface{})
+		if d["host"].(string) != "localhost" {
+			t.Error("Wrong host")
+		}
+		switch d["key"].(string) {
+		case "int":
+			if d["value"].(string) != "42" {
+				t.Errorf("Wrong value %#v", d)
+			}
+		case "f0":
+			if d["value"].(string) != "0.000000" {
+				t.Errorf("Wrong value %#v", d)
+			}
+		case "f1":
+			if d["value"].(string) != "1.000000" {
+				t.Errorf("Wrong value %#v", d)
+			}
+		case "float32":
+			if d["value"].(string) != "3.141593" {
+				t.Errorf("Wrong value %#v", d)
+			}
+		case "float64":
+			if d["value"].(string) != "3.141593" {
+				t.Errorf("Wrong value %#v", d)
+			}
+		case "s":
+			if d["value"].(string) != "string" {
+				t.Errorf("Wrong value %#v", d)
+			}
+		case "e":
+			if d["value"].(string) != "error" {
+				t.Errorf("Wrong value %#v", d)
+			}
+		default:
+			t.Fatal("default reached")
+		}
+	}
+}
