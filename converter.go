b21da1bcf24b0443069ff44f0b2612e72c833207
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2013-02-22 13:59:21 +0400

    Remove WIP.

---
+++ converter.go
@@ -0,0 +1,70 @@
+package zabbix_sender
+
+import (
+	"bytes"
+	"encoding/binary"
+	"encoding/json"
+	"fmt"
+	"time"
+)
+
+// Converts value to format accepted by Zabbix server.
+// It uses "%.6f" format for floats,
+// and fmt.Sprint() (which will try String() and Error() methods) for other types.
+func ConvertValue(i interface{}) string {
+	switch v := i.(type) {
+	case float32:
+		return fmt.Sprintf("%.6f", v)
+	case float64:
+		return fmt.Sprintf("%.6f", v)
+	default:
+		return fmt.Sprint(v)
+	}
+	panic("not reached")
+}
+
+// Single Zabbix data item.
+type DataItem struct {
+	Hostname  string `json:"host"`
+	Key       string `json:"key"`
+	Timestamp int64  `json:"clock,omitempty"` // UNIX timestamp, 0 is ignored
+	Value     string `json:"value"`           // Use ConvertValue() to fill
+}
+
+type DataItems []DataItem
+
+// Convert key/value pairs to DataItems using ConvertValue().
+// Each DataItem's Host is set to hostname, Timestamp is zeroed.
+func MakeDataItems(kv map[string]interface{}, hostname string) DataItems {
+	di := make(DataItems, len(kv))
+	i := 0
+	for k, v := range kv {
+		di[i] = DataItem{hostname, k, 0, ConvertValue(v)}
+		i++
+	}
+
+	return di
+}
+
+// Converts filled DataItems to format accepted by Zabbix server.
+// It's like dense JSON with binary header, somewhat documented there:
+// https://www.zabbix.com/documentation/1.8/protocols/agent
+func (di DataItems) Marshal() (b []byte, err error) {
+	d, err := json.Marshal(di)
+	if err == nil {
+		// the order of fields in this "JSON" is important - request should be before data
+		now := fmt.Sprint(time.Now().Unix())
+		datalen := uint64(len(d) + len(now) + 42) // 32 + d + 9 + now + 1
+		b = make([]byte, 0, datalen+13)           // datalen + 5 + 8
+		buf := bytes.NewBuffer(b)
+		buf.WriteString("ZBXD\x01")                           // 5
+		err = binary.Write(buf, binary.LittleEndian, datalen) // 8
+		buf.WriteString(`{"request":"sender data","data":`)   // 32
+		buf.Write(d)                                          // d
+		buf.WriteString(`,"clock":`)                          // 9
+		buf.WriteString(now)                                  // now
+		buf.WriteByte('}')                                    // 1
+		b = buf.Bytes()
+	}
+	return
+}
---
+++ converter_test.go
@@ -0,0 +1,126 @@
+package zabbix_sender_test
+
+import (
+	. "."
+	"bytes"
+	"encoding/binary"
+	"encoding/json"
+	"fmt"
+	"strings"
+	"testing"
+)
+
+func TestMakeDataItems(t *testing.T) {
+	var (
+		f0  float32 = 0
+		f1  float32 = 1
+		f32 float32 = 3.141592653
+		f64 float64 = 3.141592653
+		i   int     = 42
+		s   string  = "string"
+		e   error   = fmt.Errorf("error")
+		p0  *string
+	)
+	data := map[string]interface{}{"f0": f0, "f1": f1, "float32": f32, "float64": f64,
+		"int": i, "s": s, "e": e, "p0": p0}
+
+	for _, d := range MakeDataItems(data, "localhost") {
+		if d.Hostname != "localhost" {
+			t.Error("Wrong hostname")
+		}
+		if d.Timestamp != 0 {
+			t.Error("Wrong timestamp")
+		}
+		switch d.Key {
+		case "f0":
+			if d.Value != "0.000000" {
+				t.Errorf("Wrong value %#v", d)
+			}
+		case "f1":
+			if d.Value != "1.000000" {
+				t.Errorf("Wrong value %#v", d)
+			}
+		case "float32":
+			if d.Value != "3.141593" {
+				t.Errorf("Wrong value %#v", d)
+			}
+		case "float64":
+			if d.Value != "3.141593" {
+				t.Errorf("Wrong value %#v", d)
+			}
+		case "int":
+			if d.Value != "42" {
+				t.Errorf("Wrong value %#v", d)
+			}
+		case "s":
+			if d.Value != "string" {
+				t.Errorf("Wrong value %#v", d)
+			}
+		case "e":
+			if d.Value != "error" {
+				t.Errorf("Wrong value %#v", d)
+			}
+		case "p0":
+			if d.Value != "<nil>" {
+				t.Errorf("Wrong value %#v", d)
+			}
+		default:
+			t.Fatal("default reached")
+		}
+	}
+}
+
+func TestMarshal(t *testing.T) {
+	var (
+		i   int     = 42
+		f0  float32 = 0
+		f1  float32 = 1
+		f32 float32 = 3.141592653
+		f64 float64 = 3.141592653
+		s   string  = "string"
+		e   error   = fmt.Errorf("error")
+	)
+	di := MakeDataItems(
+		map[string]interface{}{"int": i, "f0": f0, "f1": f1, "float32": f32, "float64p": &f64, "s": s, "e": e},
+		"localhost",
+	)
+	b, err := di.Marshal()
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("%s", b)
+	if len(b) != cap(b) {
+		t.Errorf("Optimize: len(%d) != cap(%d)", len(b), cap(b))
+	}
+
+	if string(b[0:5]) != "ZBXD\x01" {
+		t.Error("Wrong header")
+	}
+	var datalen uint64
+	err = binary.Read(bytes.NewBuffer(b[5:13]), binary.LittleEndian, &datalen)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if datalen != uint64(len(b))-13 {
+		t.Errorf("Wrong size: %d (expected %d)", datalen, len(b)-13)
+	}
+
+	b = b[13:]
+	str := string(b)
+	if !strings.HasPrefix(str, `{"request":"sender data","data":[{`) ||
+		strings.Contains(str, `, `) ||
+		strings.Contains(str, `: `) {
+		t.Error("Zabbix's JSON parser will not parse it:", str)
+	}
+
+	var m map[string]interface{}
+	err = json.Unmarshal(b, &m)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func ExampleMakeDataItems() {
+	data := map[string]interface{}{"rpm": 42.12, "errors": 1}
+	MakeDataItems(data, "localhost")
+}
