c1f9f5d805124987933a94d5b5276b8dfa09e1a5
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2014-09-02 16:44:07 +0400

    Better documentation formatting.

--- doc.go
+++ doc.go
@@ -1,6 +1,7 @@
 // Package zabbix_sender provides interface to push data to Zabbix server's trapper items.
-// (See https://www.zabbix.com/documentation/2.0/manual/config/items/itemtypes/trapper).
-// It works similar to Zabbix's own zabbix_sender
-// (https://www.zabbix.com/documentation/2.0/manpages/zabbix_sender).
+//
+// See https://www.zabbix.com/documentation/2.0/manual/config/items/itemtypes/trapper.
+// It works similar to Zabbix's own zabbix_sender:
+// https://www.zabbix.com/documentation/2.0/manpages/zabbix_sender.
 // It should work with Zabbix 1.8 and 2.0.
 package zabbix_sender
