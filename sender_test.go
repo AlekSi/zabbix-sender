be4069e0ba03642a8ec043637f35b741dafdd335
khannz <pavel.fiskovich@gmail.com>
2017-04-10 22:53:32 +0300

    Fixed _test files

--- sender_test.go
+++ sender_test.go
@@ -5,11 +5,12 @@ import (
 	"net"
 
 	. "."
+	"time"
 )
 
 func ExampleSend() {
 	data := map[string]interface{}{"rpm": 42.12, "errors": 1}
-	di := MakeDataItems(data, "localhost")
+	di := MakeDataItems(data, "localhost", time.Now(), time.Now())
 	addr, _ := net.ResolveTCPAddr("tcp", "localhost:10051")
 	res, _ := Send(addr, di)
 	fmt.Print(res)
